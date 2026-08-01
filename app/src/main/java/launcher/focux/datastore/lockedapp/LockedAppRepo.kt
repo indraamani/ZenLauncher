0014c7d5343c84b60393c7625431e307bd431583
indraamani <indraamani24@gmail.com>
2026-08-01 22:44:08 +0530

    add: app locking datastore

---
+++ app/src/main/java/launcher/focux/datastore/lockedapp/LockedAppRepo.kt
@@ -0,0 +1,46 @@
+package launcher.focux.datastore.lockedapp
+
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import java.time.LocalTime
+
+
+class LockedAppRepo(
+    private val context: Context
+) {
+    val lockedApps : Flow<List<LockedAppModel>> by context.lockedAppDatastore.data
+        .map { app -> app.appList }
+
+    private fun hasTimePassed(initialTime: String, min: Long) : Boolean {
+        val initialTime = LocalTime.parse(initialTime)
+        initialTime.plusMinutes(min)
+        val currentTime = LocalTime.now()
+
+        return currentTime.isAfter(initialTime)
+    }
+
+    suspend fun add(lockedApp: LockedApp) {
+        context.lockedAppDatastore.updateData { current ->
+            current.copy(appList = (current.appList + lockedApp))
+        }
+
+    }
+
+    suspend fun remove(packageName: String) {
+        context.lockedAppDatastore.updateData { current ->
+            val list = current.appList.toMutableList().filter {
+                hasTimePassed(it.initialTime, it.lockedTime)
+            }
+            current.copy(list)
+        }
+    }
+
+    suspend fun hasTimePassed(packageName: String) : Boolean{
+        val list = context.lockedAppDatastore.data.first().appList
+        return list.any {
+            it.packageName == packageName && hasTimePassed(it.initialTime, it.lockedTime)
+        }
+    }
+}
